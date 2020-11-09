8856d3ef24c7083f0c7d252b60bd269524650841
babylion <btarialiya93@gmail.com>
2020-11-09 13:28:20 +0700

    freeze animation with keyboard and mouse click

--- main.js
+++ main.js
@@ -73,10 +73,31 @@ function main() {
     var uDy = gl.getUniformLocation(shaderProgram, 'dy');
     var uDz = gl.getUniformLocation(shaderProgram, 'dz');
 
+    // Element interaktif
+    var freeze = false;
+    function onMouseClick(event) {
+        freeze = !freeze;
+    }
+    document.addEventListener('click', onMouseClick, false);
+
+    function onKeyDown(event) {
+        if(event.keyCode == 32)
+            freeze = true;
+    }
+
+    function onKeyUp(event) {
+        if(event.keyCode == 32)
+            freeze = false;
+    }
+    document.addEventListener('keydown', onKeyDown, false);
+    document.addEventListener('keyup', onKeyUp, false);
+
     function render() {
-        dx += 0.001;
-        dy += 0.001;
-        dz += 0.001;
+        if(!freeze){
+            dx += 0.001;
+            dy += 0.001;
+            dz += 0.001;
+        }
         gl.uniform1f(uDx, dx);
         gl.uniform1f(uDy, dy);
         gl.uniform1f(uDz, dz);
